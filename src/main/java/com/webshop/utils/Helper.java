a0d4b1de132e408ab1961e92f01eceda8ed9fe29
shantosh <shantosh25@hotmail.com>
2024-11-22 08:24:29 +0530

    added code for capturing screenshot

--- src/main/java/com/webshop/utils/Helper.java
+++ src/main/java/com/webshop/utils/Helper.java
@@ -1,6 +1,28 @@
 package com.webshop.utils;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
 public class Helper {
+	
+	
+	public static String captureScreen(WebDriver driver)
+	{
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		String screenPath = "D:\\WORKSPACE\\BATCH31_FRAME\\com.wshop.automation\\report\\screenshot\\screen.png";
+		try {
+			FileHandler.copy(src, new File(screenPath));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return screenPath;
+	}
 	
 	public static void sleep()
 	{
